d33c844183bb952ed310339b51bf3884f2eecd3d
Lucas Damasceno <lucas.damasceno.dev@gmail.com>
2025-10-01 17:26:02 +0000

    :wrench: fix(build): Correctly separate client and server execution from single JAR

--- src/main/java/api/ServerSynchronizationController.java
+++ src/main/java/api/ServerSynchronizationController.java
@@ -19,6 +19,9 @@ import java.util.Set;
 
 import org.springframework.context.annotation.Profile;
 
+import org.springframework.context.annotation.Profile;
+
+@Profile("server")
 @RestController
 @RequestMapping("/api")
 public class ServerSynchronizationController {
